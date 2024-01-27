e82cc1f5d09f8d5b8c396d011eafb8a255081a98
Ajay Kumar <aky22896@gmail.com>
2024-01-27 21:29:06 +0530

    learned about get api, using path variable

--- src/main/java/com/example/learninglombook/controller/BeerController.java
+++ src/main/java/com/example/learninglombook/controller/BeerController.java
@@ -5,9 +5,7 @@ import com.example.learninglombook.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -15,20 +13,22 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
 
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping()
     public List<Beer> getBeerList(){
         log.debug("getting all the beer list in controller");
         return beerService.getBeerList();
     }
 
 
-    public Beer getBeerById(UUID id){
+    @RequestMapping(value = "{beerId}",method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable UUID beerId){
         log.debug("Get Beer By id in the controller layer");
-        return  beerService.getBeerById(id);
+        return  beerService.getBeerById(beerId);
     }
 }
